680b05a540e874823c1b4389d6b56fab1840f2b6
sumendra-manandhar <thesume.2010@gmail.com>
2024-10-07 05:10:38 +0545

    Initial push

--- components/Profilers.tsx
+++ components/Profilers.tsx
@@ -18,42 +18,42 @@ const Profilers = () => {
         </div>
         <div className="container md:pb-10 py-20 px-2 md:pt-36">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-16 w-11/12 mx-auto">
-            {["mid1", "mid2", "mid3"].map((coffee, index) => (
-              <div
-                key={coffee}
-                className="shadow-lg hover:shadow-xl cursor-pointer px-8 md:px-12 py-8 rounded-lg hover:scale-105 transition-all relative"
-              >
-                <div className="grid grid-cols-2 gap-6 items-end">
-                  <div className="relative">
-                    <Image
+            {/* {["mid1", "mid2", "mid3"].map((coffee, index) => ( */}
+            <div
+              // key={coffee}
+              className="shadow-lg hover:shadow-xl cursor-pointer px-8 md:px-12 py-8 rounded-lg hover:scale-105 transition-all relative"
+            >
+              <div className="grid grid-cols-2 gap-6 items-end">
+                <div className="relative">
+                  {/* <Image
                       loading="lazy"
                       width={280}
                       height={280}
                       className="absolute -right-8 bottom-[22rem] md:bottom-64 md:w-3/4 w-2/3"
                       src={`/coffee/${coffee}.png`}
                       alt={`Coffee ${index + 1}`}
-                    />
-                  </div>
-                  <div>
-                    <p className="text-gray-500 text-lg">#1 Selling</p>
-                    <h1 className="text-3xl py-2 font-semibold text-gray-700">
-                      Double Espresso
-                    </h1>
-                  </div>
+                    /> */}
                 </div>
-                <p className="py-3 text-gray-500 text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Impedit dicta alias id, nostrum rerum cum ducimus omnis sequi
-                  cumque nam.
-                </p>
-                <div className="grid md:grid-cols-2 gap-6 items-center py-2">
-                  <h1 className="font-semibold text-2xl">$59.99</h1>
-                  <button className="inline-flex items-center justify-center whitespace-nowrap ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 p-6 px-10 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide">
-                    Order Now
-                  </button>
+                <div>
+                  <p className="text-gray-500 text-lg">#1 Selling</p>
+                  <h1 className="text-3xl py-2 font-semibold text-gray-700">
+                    Double Espresso
+                  </h1>
                 </div>
               </div>
-            ))}
+              <p className="py-3 text-gray-500 text-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                dicta alias id, nostrum rerum cum ducimus omnis sequi cumque
+                nam.
+              </p>
+              <div className="grid md:grid-cols-2 gap-6 items-center py-2">
+                <h1 className="font-semibold text-2xl">$59.99</h1>
+                <button className="inline-flex items-center justify-center whitespace-nowrap ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 p-6 px-10 rounded-md shadow-lg hover:scale-105 transition-all text-lg font-normal tracking-wide">
+                  Order Now
+                </button>
+              </div>
+            </div>
+            {/* ))} */}
           </div>
         </div>
       </div>
